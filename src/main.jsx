a8f221b184aa4aed729529bc889f0c22fdc13eb9
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-04-27 11:57:37 +0300

    sss

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import "../src/index.css"
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
