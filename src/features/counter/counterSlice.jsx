64c0e21f5daebc4a26b9fa88a3e4e4a9ef0ccf97
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-04-27 09:52:01 +0300

    asdasas

--- src/features/counter/counterSlice.jsx
+++ src/features/counter/counterSlice.jsx
@@ -1,20 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    todos:[]
+const dataForm = () => {
+    return (
+      JSON.parse(localStorage.getItem("todos")) || {
+        todos: [],
+      }
+    );
 }
+
+
 
 export const counterSlice = createSlice({
   name: "todos",
-  initialState,
+  initialState:dataForm,
   reducers: {
     addTodos: (state,{payload}) => {
         state.todos.push(payload)
+        localStorage.setItem("todos",JSON.stringify(state))
     },
     removeTodos: (state,{payload}) => {
         state.todos = state.todos.filter((todo) => {
             return todo.id !== payload
         })
+        localStorage.setItem("todos", JSON.stringify(state));
+
     },
   },
 });
